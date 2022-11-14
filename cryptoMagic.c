96afd0f3a4573ebc47d24ba7dbe4d7958ae38796
Javier Lazo-Arevalo <javieralejandrolazoarevalo@gmail.com>
2022-11-14 09:50:07 -0500

    pushing correct cryptoMagic

--- cryptoMagic.c
+++ cryptoMagic.c
@@ -4,30 +4,6 @@
 #include <string.h>
 int main(int argc, char *argv[])
 {
-    /*
-    int i=0;
-    FILE* textfile;  //creates a variable to call and manipulate the file
-    char character;
-    textfile=fopen("read_test.txt", "r");    //opens the file to read
-    if (NULL == textfile)
-    {
-        printf("file can't be opened \n");
-    }
-    //if statement above checks if the file exists
-    do
-    {
-        character = fgetc(textfile); //gets a single character
-        printf("%c", character);
-    }
-    while (character != EOF);
-    //do while statement to read the file while the end of file has not been reached
-    fclose(textfile);    //coles the file
-    
-    textfile=fopen("read_test.txt", "w");    //opens the file to write
-    fprintf(textfile, "This is a string which is written to a file\n");  //writes to file
-    fclose(textfile);    //closes file
-    */
-    
     if ((strcmp(argv[1],"-E") == 0)) //if the comand promt is not D it will encrypt
     {
         //encryption function
