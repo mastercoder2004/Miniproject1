a254c49001ae72a7fa6da3a665c0a5bed28adcbf
Javier Lazo-Arevalo <javieralejandrolazoarevalo@gmail.com>
2022-11-14 10:31:46 -0500

    added the attempted encryption and decryption functions

--- cryptoMagic.c
+++ cryptoMagic.c
@@ -7,6 +7,38 @@ int main(int argc, char *argv[])
     if ((strcmp(argv[1],"-E") == 0)) //if the comand promt is not D it will encrypt
     {
         //encryption function
+        /*
+    FILE* original;
+    FILE* encrypt;
+    original=fopen("original.txt", "r");
+    encrypt=fopen("encrypt.txt", "a");
+     int outChar;
+     char letter;
+    do
+    {
+        letter = fgetc(original);
+        outChar= (int) letter;
+        if(outChar==9)
+        {
+            fprintf(encrypt, "TT");
+        }
+        else
+        {
+            outChar = outChar - 16;
+
+            if (outChar<32)
+            {
+                (outChar-32)+144;
+            }
+        printf("%x", outChar);
+        fprintf(encrypt, "%x",outChar);
+        }
+    }
+    while (letter != EOF);
+    fclose(original);
+    fclose(encrypt);
+    */
+
         int length1=0;   //initiallizes size of file name before file extension
         char *file_name1=argv[2];    //sets a prointer to where the file name should be
         int size1;   //initiallizes size of the file name and file extension
@@ -44,6 +76,48 @@ int main(int argc, char *argv[])
     else if ((strcmp(argv[1],"-D") == 0))
     {
         //decryption function
+        /*
+        FILE* Encrypt;
+        FILE* decrypt;
+        int first, second;
+        int letter;
+        char character;
+        character = 0;
+        Encrypt=fopen("Encrypt.txt", "r");
+        decrypt=fopen("decrypt.txt", "a");
+        do
+        {
+        first = fgetc(Encrypt);
+        if(first >= 65){
+        first = first - 86;
+        }
+        else {
+        first = first- 48;
+        }
+    first = first * 16;
+    printf("%d",first);
+    second = fgetc(Encrypt);
+    if(second>=65){
+        second = second - 86;
+    }
+    else{
+   second = second - 48;
+   printf("%d",second);
+    }
+        letter = first + second + 16;
+        if(letter > 127){
+        (letter-144) + 32;
+        }
+        printf("%c", letter);
+        fprintf(decrypt, "%c",letter);
+
+
+}
+    while ((char) letter != EOF);
+fclose(Encrypt);
+fclose(decrypt);
+        */
+
         int length2=0;   //initiallizes size of file name before file extension
         char *file_name2=argv[2];    //sets a prointer to where the file name should be
         int size2;   //initiallizes size of the file name and file extension
@@ -81,6 +155,38 @@ int main(int argc, char *argv[])
     else
     {
         //encryption function
+        /*
+    FILE* original;
+    FILE* encrypt;
+    original=fopen("original.txt", "r");
+    encrypt=fopen("encrypt.txt", "a");
+     int outChar;
+     char letter;
+    do
+    {
+        letter = fgetc(original);
+        outChar= (int) letter;
+        if(outChar==9)
+        {
+            fprintf(encrypt, "TT");
+        }
+        else
+        {
+            outChar = outChar - 16;
+
+            if (outChar<32)
+            {
+                (outChar-32)+144;
+            }
+        printf("%x", outChar);
+        fprintf(encrypt, "%x",outChar);
+        }
+    }
+    while (letter != EOF);
+    fclose(original);
+    fclose(encrypt);
+    */
+   
         int length1=0;   //initiallizes size of file name before file extension
         char *file_name1=argv[1];    //sets a prointer to where the file name should be
         int size1;   //initiallizes size of the file name and file extension
